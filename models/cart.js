1e5d8f364d1aa7ef0190da4c4674a6fd33105eb7
Sung Hyun Hong (Jenny) <57204918+Jenny9197@users.noreply.github.com>
2021-10-04 09:09:16 +0900

    Delete models directory

--- models/cart.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const { Schema } = mongoose;
-const cartSchema = new Schema({
-  userId: { type: String, required: true, unique: true },
-  goodsId: { type: String, required: true, unique: false },
-  quantity: {type: Number, required: true, unique: false },
-});
-module.exports = mongoose.model("carts", cartSchema);
--- models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const { Schema } = mongoose;
-const userSchema = new Schema({
-  userId: { type: String, required: true, unique: true },
-  password: { type: String, required: true, unique: false },
-  nickname: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: false },
-});
-module.exports = mongoose.model("users", userSchema);
